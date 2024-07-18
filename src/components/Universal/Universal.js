fe9c159466babae12b6f46dd6ca68d1242789b6e
Mary Salvatore <marysalvatore084@gmail.com>
2024-07-18 19:56:10 -0400

    pushing updates

--- src/components/Universal/Universal.js
+++ src/components/Universal/Universal.js
@@ -126,7 +126,7 @@ const Universal = ({data}) => {
             <div className="" style={{display: 'flex'}}>
               <Image src={arrow_left} height="20" width="20" /><input name='email' value={email} className="w-full" type="text" disabled style={{background: "white", border: "none"}}  />
             </div>
-            <h4 style={{fontWeight: "800"}}>Enter Password</h4>
+            <h4 style={{fontWeight: "800", color: 'gray'}}>Enter Password</h4>
 
             <div class="relative z-0 w-full mb-5 mt-1 group ">
                 <input type="password" value={password} onChange={e => setPassword(e.target.value)} name="password" id="password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
